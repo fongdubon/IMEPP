04571bee9dcae3aed9037bc3fa993c9a2a54f94d
Eduardo Fong Dubon <eduardofongdubon@hotmail.com>
2021-04-29 12:14:17 -0500

    Revert "Se hizo el diseño de Adviser y la funcionalidad de los botones"
    This reverts commit 99c9aedccc8f7ec76e72dae8c13aa1996b8290af.

--- IMEPP/Entities/Adviser.cs
+++ IMEPP/Entities/Adviser.cs
@@ -1,22 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace IMEPP.Entities
+﻿namespace IMEPP.Entities
 {
     public class Adviser
     {
-        #region Propiedades
-        [Key]
         public int Id { get; set; }
-        [Required]
-        [StringLength(100)]
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string CellphoneNumber { get; set; }
-        public string Email { get; set; }
-        public string Photo { get; set; }
-        [StringLength(200)]
-        public string Department { get; set; }
-        #endregion
     }
 }
