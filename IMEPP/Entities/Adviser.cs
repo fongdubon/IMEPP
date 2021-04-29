99c9aedccc8f7ec76e72dae8c13aa1996b8290af
Ixe Pompa <pomparesultados@outlook.com>
2021-04-29 02:17:12 -0500

    Se hizo el diseño de Adviser y la funcionalidad de los botones

--- IMEPP/Entities/Adviser.cs
+++ IMEPP/Entities/Adviser.cs
@@ -1,7 +1,22 @@
-﻿namespace IMEPP.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMEPP.Entities
 {
     public class Adviser
     {
+        #region Propiedades
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CellphoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+        [StringLength(200)]
+        public string Department { get; set; }
+        #endregion
     }
 }
